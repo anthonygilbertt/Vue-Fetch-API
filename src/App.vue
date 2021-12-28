effe5d9404aa5c38bf344a25b886655dabdc73e0
Anthony <anthony.gilbert93@gmail.com>
2021-12-28 15:13:05 -0600

    Added fetch Get request to NASA SPOD endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" id="logo" />
+  <Main />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Main,
+  },
+};
 </script>
 
 <style>
@@ -22,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo {
+  height: 100px;
 }
 </style>
