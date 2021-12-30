fbd9a92427f9b2c010436050b99195f29c15a71d
Anthony <anthony.gilbert93@gmail.com>
2021-12-30 15:26:26 -0600

    Created a generic chart for the CSV using chart.js

--- src/components/FetchCSV.vue
+++ src/components/FetchCSV.vue
@@ -4,6 +4,8 @@
     <div>
       <div v-if="video"></div>
       <div v-else></div>
+      <!-- <canvas id="myChart" width="400" height="400"></canvas> -->
+      <canvas id="myChart" width="400" height="400"></canvas>
     </div>
 
     <button @click="fetchData">Fetch Data</button>
@@ -12,6 +14,8 @@
 
 <script>
 import { ref } from "vue";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 export default {
   name: "Main",
@@ -20,33 +24,100 @@ export default {
     const video = ref(false);
 
     const fetchData = async () => {
-      //   try {
-      // fetch("/assets/Zonal-Annual-Means.csv", {
-      //     fetch("test.csv", {
-      //       method: "GET",
-      //     })
-      //       .then((res) => {
-      //         console.log(res);
-      //         return res.text();
-      //       })
-      //       .then((data) => {
-      //         console.log(data);
-      //       });
-      //     error.value = null;
-      //   } catch (err) {
-      //     error.value = err;
-      //   }
-
-      // getData();
-      // async function getData() {
-      // const response = await fetch(".Zonal-Annual-Means.csv");
       const response = await fetch("Zonal-Annual-Means.csv");
       const data = await response.text();
-      console.log(data);
-      // }
+      const table = data.split("\n").slice(1);
+
+      // console.table("table: ", table);
+
+      table.forEach((row) => {
+        const columns = row.split(",");
+        const year = columns[0];
+        const temp = columns[1];
+        console.log(year, temp);
+      });
+
+      const ctx = document.getElementById("myChart");
+
+      const myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+      console.log(myChart);
+      //end of myChart function
     };
 
-    return { fetchData, video, error };
+    // async function chartIt() {
+    //   // Organize the `chart.js` related code line into a function
+    //   const data = await fetchData(); // Get resulting data from the getData() function.
+    //   const ctx = document.getElementById("myChart").getContext("2d");
+    //   new Chart(ctx, {
+    //     type: "line", // Switch the graph to a line chart
+    //     data: {
+    //       labels: data.xs, // Customize the label sources
+    //       datasets: [
+    //         {
+    //           label:
+    //             "Combined Land-Surface Air and Sea-Surface Water Temperature (℃)", // new name
+    //           data: data.ys, // Customize the data source for the Y-axis
+    //           fill: false, // Do not fill the line
+    //           backgroundColor: "rgba(255, 99, 132, 0.2)", // Adopt single color scheme across different datasets
+    //           borderColor: "rgba(255, 99, 132, 1)", // adopt single color scheme across different datasets
+    //           borderWidth: 1,
+    //         },
+    //       ],
+    //     },
+    //     options: {
+    //       scales: {
+    //         yAxes: [
+    //           {
+    //             ticks: {
+    //               // Include a ℃ sign in the ticks
+    //               callback: function (value, index, values) {
+    //                 return value + "℃";
+    //               },
+    //             },
+    //           },
+    //         ],
+    //       },
+    //     },
+    //   });
+    // }
+
+    // return { fetchData, video, error, chartIt };
+    return { video, error, fetchData };
   },
 };
 </script>
