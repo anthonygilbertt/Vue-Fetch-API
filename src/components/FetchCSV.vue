c1a91fef4a8363a8fe878fbbeb25d9c1b8a776d2
Anthony <anthony.gilbert93@gmail.com>
2021-12-30 14:30:55 -0600

    Set up preloading of CSV asset

---
+++ src/components/FetchCSV.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="csvdata">
+    <h3>NASA:</h3>
+    <div>
+      <div v-if="video"></div>
+      <div v-else></div>
+    </div>
+
+    <button @click="fetchData">Fetch Data</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Main",
+  setup() {
+    const error = ref(null);
+    const video = ref(false);
+
+    const fetchData = async () => {
+      //   try {
+      // fetch("/assets/Zonal-Annual-Means.csv", {
+      //     fetch("test.csv", {
+      //       method: "GET",
+      //     })
+      //       .then((res) => {
+      //         console.log(res);
+      //         return res.text();
+      //       })
+      //       .then((data) => {
+      //         console.log(data);
+      //       });
+      //     error.value = null;
+      //   } catch (err) {
+      //     error.value = err;
+      //   }
+
+      // getData();
+      // async function getData() {
+      // const response = await fetch(".Zonal-Annual-Means.csv");
+      const response = await fetch("Zonal-Annual-Means.csv");
+      const data = await response.text();
+      console.log(data);
+      // }
+    };
+
+    return { fetchData, video, error };
+  },
+};
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+h3 {
+  text-decoration: underline;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <Fetch />
+    <FetchCSV />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Fetch from "@/components/Fetch.vue";
+import FetchCSV from "@/components/FetchCSV.vue";
 
 export default {
   name: "Home",
   components: {
     Fetch,
+    FetchCSV,
   },
 };
 </script>
