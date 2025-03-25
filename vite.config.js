a90808e87a32fcf34f3777d087b8feb05889df1e
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-24 22:38:09 -0500

    Added a node version to the package.json file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-const { defineConfig } = require('vite')
-const vue = require('@vitejs/plugin-vue')
-const path = require('path')
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-module.exports = defineConfig({
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  define: {
+    'process.env': {}
   }
 })
