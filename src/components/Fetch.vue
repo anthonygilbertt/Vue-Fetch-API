628223ead013c4b0962aa75dd0667bcbe4c42c22
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-24 23:06:17 -0500

    I think I fixed the issue with videos now rendering

--- src/components/Fetch.vue
+++ src/components/Fetch.vue
@@ -12,18 +12,25 @@
         <b><u>URL:</u></b> {{ path }}
       </p>
 
-      <div v-if="video">
+      <div v-if="video && embedUrl">
+        <p><b>Video Content:</b></p>
         <iframe
           width="560"
           height="315"
-          :src="path"
+          :src="embedUrl"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
       </div>
-      <div v-else><img :src="path" alt="" height="400" /></div>
+      <div v-else-if="path">
+        <p><b>Image Content:</b></p>
+        <img :src="path" alt="" height="400" />
+      </div>
+    </div>
+    <div v-else>
+      <p>Error: {{ error }}</p>
     </div>
 
     <button class="button" @click="fetchData">Fetch Data</button>
@@ -31,7 +38,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 export default {
   name: "Main",
@@ -42,6 +49,25 @@ export default {
     const error = ref(null);
     const video = ref(false);
 
+    const embedUrl = computed(() => {
+      if (!path.value) return '';
+      // Handle youtube.com/watch?v= format
+      if (path.value.includes('youtube.com/watch?v=')) {
+        const videoId = path.value.split('v=')[1].split('&')[0];
+        return `https://www.youtube.com/embed/${videoId}`;
+      }
+      // Handle youtu.be/ format
+      if (path.value.includes('youtu.be/')) {
+        const videoId = path.value.split('youtu.be/')[1];
+        return `https://www.youtube.com/embed/${videoId}`;
+      }
+      // If it's already an embed URL, return as is
+      if (path.value.includes('youtube.com/embed/')) {
+        return path.value;
+      }
+      return path.value;
+    });
+
     const fetchData = () => {
       try {
         fetch(
@@ -50,23 +76,32 @@ export default {
             method: "GET",
           }
         )
-          .then((res) => {
-            console.log(res);
-            return res.json();
-          })
+          .then((res) => res.json())
           .then((blob) => {
-            console.log(blob);
+            console.log('APOD Response:', blob);
+            console.log('Media Type:', blob.media_type);
+            console.log('URL:', blob.url);
+            
             title.value = blob.title;
             explanation.value = blob.explanation;
             path.value = blob.url;
+            
+            // Use the media_type from the API response
+            video.value = blob.media_type === 'video';
+            console.log('Is Video:', video.value);
+            console.log('Computed Embed URL:', embedUrl.value);
+          })
+          .catch((err) => {
+            console.error('Fetch Error:', err);
+            error.value = err;
           });
-        error.value = null;
       } catch (err) {
+        console.error('Try-Catch Error:', err);
         error.value = err;
       }
     };
 
-    return { title, explanation, fetchData, path, video };
+    return { title, explanation, fetchData, path, video, embedUrl, error };
   },
 };
 </script>
