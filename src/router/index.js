4adb5548ef150e57c2438d21f94fb44005a8d05a
Anthony Gilbert <anthony.gilbert93@gmail.com>
2025-03-24 22:51:48 -0500

    Removed the Put.vue component

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,6 @@ const routes = [{
         path: '/',
         name: 'Home',
         component: Home
-    },
-    {
-        path: '/put',
-        name: 'Put',
-        // route level code-splitting
-        // this generates a separate chunk (put.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ('../views/Put.vue')
     }
 ]
 
